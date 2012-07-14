368c2b8687af48450847fbdb9acdbfbd451e7a01
yaguchi <murooka.1129.x50@gmail.com>
2012-07-14 19:13:58 +0900

    modify ai.cpp

--- events/ICFPC2012/ai.cpp
+++ events/ICFPC2012/ai.cpp
@@ -2,23 +2,31 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <locale>
+#include <queue>
 
 
 using namespace std;
 
-class Game {
-    public Field field;
-};
+CommandType commands[] = { LEFT, RIGHT, UP, DOWN, WAIT, ABORT, DUMMY };
 
-
-class Field {
-    
-};
-
+long str_hash(const string& str);
 
 int main(int argc, char **argv)/*{{{*/
 {
+    Game game; // get map data from standard input
+    queue<Game> game_queue;
+    game_queue.push(game);
+    while (!game_queue.empty()) {
+        Game g = game_queue.front(); game_queue.pop();
+        g.move();
+    }
 
     return 0;
 }/*}}}*/
+
+static locale loc;
+static const collate<char>& coll = use_facet<collate<char> >(loc);
+long str_hash(const string& str) {
+    return coll.hash(str.data(), str.data()+str.length());
+}
