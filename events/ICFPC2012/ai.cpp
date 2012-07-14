fb6cd675c2122a69c05e56d5bfc0a54df2bb2423
yaguchi <murooka.1129.x50@gmail.com>
2012-07-14 18:02:33 +0900

    add ai.ccp and commands.txt

---
+++ events/ICFPC2012/ai.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+
+using namespace std;
+
+class Game {
+    public Field field;
+};
+
+
+class Field {
+    
+};
+
+
+int main(int argc, char **argv)/*{{{*/
+{
+
+    return 0;
+}/*}}}*/
