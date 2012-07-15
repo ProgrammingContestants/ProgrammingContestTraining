c242f2fffbdbc6214854d0912e52809cd1c269c4
Kazunori Sakamoto <exkazuu@gmail.com>
2012-07-15 16:52:55 +0900

    Add test case for moving.

--- events/ICFPC2012/simulator/test/BeardMapTest.cpp
+++ events/ICFPC2012/simulator/test/BeardMapTest.cpp
@@ -9,6 +9,9 @@ TEST(BeardMapTest, map1_parse) {
   Field &field = game.get_field();
   ASSERT_EQ(10, field.get_width());
   ASSERT_EQ(10, field.get_height());
+  Metadata &m = game.get_metadata();
+  ASSERT_EQ(15, m.get_growth());
+  ASSERT_EQ(0, m.get_razors());
 }
 
 TEST(BeardMapTest, map2_parse) {
@@ -17,6 +20,10 @@ TEST(BeardMapTest, map2_parse) {
   Field &field = game.get_field();
   ASSERT_EQ(30, field.get_width());
   ASSERT_EQ(15, field.get_height());
+  Metadata &m = game.get_metadata();
+  ASSERT_EQ(25, m.get_growth());
+  ASSERT_EQ(10, m.get_razors());
+  ASSERT_EQ(20, m.get_flooding());
 }
 
 TEST(BeardMapTest, map3_parse) {
@@ -25,6 +32,9 @@ TEST(BeardMapTest, map3_parse) {
   Field &field = game.get_field();
   ASSERT_EQ(24, field.get_width());
   ASSERT_EQ(13, field.get_height());
+  Metadata &m = game.get_metadata();
+  ASSERT_EQ(10, m.get_growth());
+  ASSERT_EQ('1', m.get_target_id('A'));
 }
 
 TEST(BeardMapTest, map4_parse) {
