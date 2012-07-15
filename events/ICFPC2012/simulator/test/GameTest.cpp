9eabfec889f3d461463438c28153576207a67d82
yaguchi <murooka.1129.x50@gmail.com>
2012-07-15 15:46:31 +0900

    fix GameTest

--- events/ICFPC2012/simulator/test/GameTest.cpp
+++ events/ICFPC2012/simulator/test/GameTest.cpp
@@ -6,8 +6,8 @@
 TEST(GameTest, map1_parse) {
   ifstream ifs("map/contest1.map");
   Game game(ifs);
-  Field *field = game.get_field();
-  ASSERT_EQ(6, field->get_width());
-  ASSERT_EQ(6, field->get_height());
+  Field &field = game.get_field();
+  ASSERT_EQ(6, field.get_width());
+  ASSERT_EQ(6, field.get_height());
 }
 
