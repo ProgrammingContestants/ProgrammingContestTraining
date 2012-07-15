15ad6c1b9f4935da475da45e54865022f31262de
Kazunori Sakamoto <exkazuu@gmail.com>
2012-07-15 18:27:44 +0900

    Add tests for first maps.

--- events/ICFPC2012/simulator/run_test.sh
+++ events/ICFPC2012/simulator/run_test.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+cd ../
+make
+cd simulator
+make
 g++ -o test/test.out test/FirstMapTest.cpp src/*.o ../gtest-1.6.0/gtest/gtest*.cc -I ../gtest-1.6.0/
 test/test.out
--- events/ICFPC2012/simulator/test/FirstMapTest.cpp
+++ events/ICFPC2012/simulator/test/FirstMapTest.cpp
@@ -3,6 +3,8 @@
 
 #include "gtest/gtest.h"
 
+// ---------------------- parse test ---------------------- //
+
 TEST(FirstMapTest, map1_parse) {
   ifstream ifs("map/contest1.map");
   Game game(ifs);
@@ -75,16 +77,135 @@ TEST(FirstMapTest, map9_parse) {
   ASSERT_EQ(17, field.get_height());
 }
 
-TEST(FirstMapTest, map1_best) {
+TEST(FirstMapTest, map10_parse) {
+  ifstream ifs("map/contest10.map");
+  Game game(ifs);
+  Field &field = game.get_field();
+  ASSERT_EQ(29, field.get_width());
+  ASSERT_EQ(24, field.get_height());
+}
+
+// ---------------------- tests for simulating moving ---------------------- //
+
+TEST(FirstMapTest, map1_play) {
   ifstream ifs("map/contest1.map");
   Game game(ifs);
-  GameState s = game.moves("LDRDDUULLLDDL");
+  GameState s = game.moves("LDRDDLRULLLDL");
   ASSERT_EQ(212, s.get_score());
 }
 
-TEST(FirstMapTest, map3_best) {
+TEST(FirstMapTest, map2_play) {
+  ifstream ifs("map/contest2.map");
+  Game game(ifs);
+  GameState s = game.moves("RRUDRRULURULLLLDDDL");
+  ASSERT_EQ(281, s.get_score());
+}
+
+TEST(FirstMapTest, map3_play) {
   ifstream ifs("map/contest3.map");
   Game game(ifs);
   GameState s = game.moves("LDDDRRRRDDLLLLLDURRRRUURR");
   ASSERT_EQ(275, s.get_score());
+}
+
+// ---------------------- tests for finding best solution and simulating moving ---------------------- //
+
+TEST(FirstMapTest, map1_best) {
+  string map_path = "map/contest1.map", cmds;
+  system(("../ai.out < " + map_path + " > ret.out 2> /dev/null").c_str());
+  ifstream ifs(map_path.c_str()), cmds_ifs("ret.out");
+  cmds_ifs >> cmds;
+  Game game(ifs);
+  GameState s = game.moves(cmds);
+  ASSERT_EQ(212, s.get_score());
+}
+
+TEST(FirstMapTest, map2_best) {
+  string map_path = "map/contest2.map", cmds;
+  system(("../ai.out < " + map_path + " > ret.out 2> /dev/null").c_str());
+  ifstream ifs(map_path.c_str()), cmds_ifs("ret.out");
+  cmds_ifs >> cmds;
+  Game game(ifs);
+  GameState s = game.moves(cmds);
+  ASSERT_EQ(281, s.get_score());
+}
+
+TEST(FirstMapTest, map3_best) {
+  string map_path = "map/contest3.map", cmds;
+  system(("../ai.out < " + map_path + " > ret.out 2> /dev/null").c_str());
+  ifstream ifs(map_path.c_str()), cmds_ifs("ret.out");
+  cmds_ifs >> cmds;
+  Game game(ifs);
+  GameState s = game.moves(cmds);
+  ASSERT_EQ(275, s.get_score());
+}
+
+TEST(FirstMapTest, map4_best) {
+  string map_path = "map/contest4.map", cmds;
+  system(("../ai.out < " + map_path + " > ret.out 2> /dev/null").c_str());
+  ifstream ifs(map_path.c_str()), cmds_ifs("ret.out");
+  cmds_ifs >> cmds;
+  Game game(ifs);
+  GameState s = game.moves(cmds);
+  ASSERT_EQ(575, s.get_score());
+}
+
+TEST(FirstMapTest, map5_best) {
+  string map_path = "map/contest5.map", cmds;
+  system(("../ai.out < " + map_path + " > ret.out 2> /dev/null").c_str());
+  ifstream ifs(map_path.c_str()), cmds_ifs("ret.out");
+  cmds_ifs >> cmds;
+  Game game(ifs);
+  GameState s = game.moves(cmds);
+  ASSERT_EQ(1303, s.get_score());
+}
+
+TEST(FirstMapTest, map6_best) {
+  string map_path = "map/contest6.map", cmds;
+  system(("../ai.out < " + map_path + " > ret.out 2> /dev/null").c_str());
+  ifstream ifs(map_path.c_str()), cmds_ifs("ret.out");
+  cmds_ifs >> cmds;
+  Game game(ifs);
+  GameState s = game.moves(cmds);
+  ASSERT_EQ(1177, s.get_score());
+}
+
+TEST(FirstMapTest, map7_best) {
+  string map_path = "map/contest7.map", cmds;
+  system(("../ai.out < " + map_path + " > ret.out 2> /dev/null").c_str());
+  ifstream ifs(map_path.c_str()), cmds_ifs("ret.out");
+  cmds_ifs >> cmds;
+  Game game(ifs);
+  GameState s = game.moves(cmds);
+  ASSERT_EQ(869, s.get_score());
+}
+
+TEST(FirstMapTest, map8_best) {
+  string map_path = "map/contest8.map", cmds;
+  system(("../ai.out < " + map_path + " > ret.out 2> /dev/null").c_str());
+  ifstream ifs(map_path.c_str()), cmds_ifs("ret.out");
+  cmds_ifs >> cmds;
+  Game game(ifs);
+  GameState s = game.moves(cmds);
+  ASSERT_EQ(1973, s.get_score());
+}
+
+TEST(FirstMapTest, map9_best) {
+  string map_path = "map/contest9.map", cmds;
+  system(("../ai.out < " + map_path + " > ret.out 2> /dev/null").c_str());
+  ifstream ifs(map_path.c_str()), cmds_ifs("ret.out");
+  cmds_ifs >> cmds;
+  Game game(ifs);
+  GameState s = game.moves(cmds);
+  ASSERT_EQ(3093, s.get_score());
+}
+
+TEST(FirstMapTest, map10_best) {
+  string map_path = "map/contest10.map", cmds;
+  system(("../ai.out < " + map_path + " > ret.out 2> /dev/null").c_str());
+  ifstream ifs(map_path.c_str()), cmds_ifs("ret.out");
+  cmds_ifs >> cmds;
+  Game game(ifs);
+  GameState s = game.moves(cmds);
+  ASSERT_EQ(3634, s.get_score());
 }
